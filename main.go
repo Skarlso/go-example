c6dc556c038a46837f685d7be77114487b75d416
Gergely Brautigam <skarlso777@gmail.com>
2020-08-06 17:03:13 +0200

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,8 @@ func CreateUser(args sdk.Arguments) error {
 	log.Println("Update11!")
 	log.Println("Update12!")
 	log.Println("Update13!")
-	log.Println("Update14!")	
+	log.Println("Update14!")
+	log.Println("Update15!")	
 
 	// lets sleep to simulate that we do something
 	time.Sleep(5 * time.Second)
