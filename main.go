61f8608333ba3013f80fde741c321b50cf4f3901
Gergely Brautigam <skarlso777@gmail.com>
2020-08-07 13:56:49 +0200

    Update main.go

--- main.go
+++ main.go
@@ -44,7 +44,8 @@ func CreateUser(args sdk.Arguments) error {
 	log.Println("Update33!")	
 	log.Println("Update34!")	
 	log.Println("Update35!")	
-	log.Println("Update36!")		
+	log.Println("Update36!")
+	log.Println("Update37!")
 	
 	// lets sleep to simulate that we do something
 	time.Sleep(5 * time.Second)
